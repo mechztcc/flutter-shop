0217501cbec4e70ae364cf94ed94eaefe252d6c8
mechztcc <dojathan@gmail.com>
2021-12-21 15:00:57 -0300

    Add cards to grid view with gridtile

---
+++ lib/components/product_item.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:shop/models/product.dart';
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+  const ProductItem({Key? key, required this.product}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: Image.network(
+          product.imageUrl,
+          fit: BoxFit.cover,
+        ),
+        footer: GridTileBar(
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.favorite),
+          ),
+          trailing: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.shopping_cart),
+          ),
+          backgroundColor: Colors.black45,
+        ),
+      ),
+    );
+  }
+}
--- lib/views/products_overview_page.dart
+++ lib/views/products_overview_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop/components/product_item.dart';
 import 'package:shop/data/dummy_data.dart';
 import 'package:shop/models/product.dart';
 
@@ -15,17 +16,16 @@ class ProductsOverviewPage extends StatelessWidget {
           'Minha Loja',
         ),
       ),
-      body: Padding(
+      body: GridView.builder(
         padding: const EdgeInsets.all(10),
-        child: GridView.builder(
-          itemCount: loadedProducts.length,
-          itemBuilder: (context, index) => Text(loadedProducts[index].title),
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-          ),
+        itemCount: loadedProducts.length,
+        itemBuilder: (context, index) =>
+            ProductItem(product: loadedProducts[index]),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
         ),
       ),
     );
