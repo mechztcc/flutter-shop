db9cad0fb417efe870fe7481df53947a4c767c04
mechztcc <dojathan@gmail.com>
2021-12-23 15:23:43 -0300

    controll favorite exibition with notifier

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -5,10 +5,27 @@ import 'package:shop/models/product.dart';
 class ProductList with ChangeNotifier {
   List<Product> _items = dummyProducts;
 
-  List<Product> get items => [..._items];
+  List<Product> get items {
+    if (_showFavoriteOnly) {
+      return _items.where((prod) => prod.isFavorite).toList();
+    }
+    return [..._items];
+  }
+
+  bool _showFavoriteOnly = false;
 
   void addProduct(Product product) {
     _items.add(product);
+    notifyListeners();
+  }
+
+  void showFavoriteOnly() {
+    _showFavoriteOnly = true;
+    notifyListeners();
+  }
+
+  void showAll() {
+    _showFavoriteOnly = false;
     notifyListeners();
   }
 }
