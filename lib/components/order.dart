9e2b2e63abdbebf50ed8c78d1ac8793dbb4b0a0b
mechztcc <dojathan@gmail.com>
2021-12-27 12:29:25 -0300

    Need a solution to show items of a order

--- lib/components/order.dart
+++ lib/components/order.dart
@@ -2,25 +2,40 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:shop/models/order.dart';
 
-class OrderWidget extends StatelessWidget {
+class OrderWidget extends StatefulWidget {
   final Order order;
 
   const OrderWidget({Key? key, required this.order}) : super(key: key);
 
   @override
+  State<OrderWidget> createState() => _OrderWidgetState();
+}
+
+class _OrderWidgetState extends State<OrderWidget> {
+  bool _expanded = false;
+
+  @override
   Widget build(BuildContext context) {
     return Card(
-      child: ListTile(
-        title: Text(
-          'R\$ ${order.total.toStringAsFixed(2)}',
-        ),
-        subtitle: Text(
-          DateFormat('dd/MM/yyyy hh:mm').format(order.date),
-        ),
-        trailing: IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.expand_more),
-        ),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text(
+              'R\$ ${widget.order.total.toStringAsFixed(2)}',
+            ),
+            subtitle: Text(
+              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.date),
+            ),
+            trailing: IconButton(
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
+              icon: Icon(Icons.expand_more),
+            ),
+          ),
+        ],
       ),
     );
   }
