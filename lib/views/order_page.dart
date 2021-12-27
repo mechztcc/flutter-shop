85343568726b40863d81ef57f6b533d39080a2ce
mechztcc <dojathan@gmail.com>
2021-12-27 11:23:48 -0300

    created order page

---
+++ lib/views/order_page.dart
@@ -0,0 +1,16 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class OrdersPage extends StatelessWidget {
+  const OrdersPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pedidos'),
+      ),
+    );
+  }
+}
