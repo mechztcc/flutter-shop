686dd6af58b8e5ff9fa26ef10b8baa09579041f9
mechztcc <dojathan@gmail.com>
2022-01-03 18:28:22 -0300

    add url imagem

--- lib/views/product_form_page.dart
+++ lib/views/product_form_page.dart
@@ -13,11 +13,27 @@ class _ProductFormPageState extends State<ProductFormPage> {
   Widget build(BuildContext context) {
     final _priceFocus = FocusNode();
     final _descriptionFocus = FocusNode();
+    final _urlFocus = FocusNode();
+
+    final _imageUrlController = TextEditingController();
+
+    void updateImage() {
+      setState(() {});
+    }
+
+    @override
+    void initState() {
+      super.initState();
+      _urlFocus.addListener(updateImage);
+    }
 
     @override
     void dispose() {
       super.dispose();
       _priceFocus.dispose();
+      _descriptionFocus.dispose();
+      _urlFocus.removeListener(updateImage);
+      _urlFocus.dispose();
     }
 
     return Scaffold(
@@ -56,6 +72,43 @@ class _ProductFormPageState extends State<ProductFormPage> {
                 focusNode: _descriptionFocus,
                 keyboardType: TextInputType.multiline,
                 maxLines: 3,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Url da Imagem',
+                      ),
+                      focusNode: _urlFocus,
+                      keyboardType: TextInputType.url,
+                      textInputAction: TextInputAction.done,
+                      controller: _imageUrlController,
+                    ),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    margin: const EdgeInsets.only(
+                      top: 10,
+                      left: 10,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 1,
+                      ),
+                    ),
+                    alignment: Alignment.center,
+                    child: _imageUrlController.text.isEmpty
+                        ? const Text('Informa a URL')
+                        : FittedBox(
+                            child: Image.network(_imageUrlController.text),
+                            fit: BoxFit.cover,
+                          ),
+                  )
+                ],
               ),
             ],
           ),
