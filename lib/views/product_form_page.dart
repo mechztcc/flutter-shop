b6035c620c9e92bcd0fd2b122943df5cabd375f3
mechztcc <dojathan@gmail.com>
2022-01-03 13:07:14 -0300

    text form

--- lib/views/product_form_page.dart
+++ lib/views/product_form_page.dart
@@ -15,6 +15,19 @@ class _ProductFormPageState extends State<ProductFormPage> {
       appBar: AppBar(
         title: Text('Formulario de Produto'),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Form(
+          child: ListView(children: [
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Nome',
+              ),
+              textInputAction: TextInputAction.next,
+            ),
+          ]),
+        ),
+      ),
     );
   }
 }
